44f5d93f73195a691cdc2242a118279ed55f2e62
anubhavbaghel23 <anubhavbaghel23@gmail.com>
2025-12-29 11:04:18 +0530

    added stickyness to footer and navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,14 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className="flex flex-col items-center py-2 px-4">
+        <div className="flex flex-col items-center py-2 px-4 sticky bottom-0 bg-white">
             <div className='flex items-center gap-2'>
                 Made with <FaHeart className='text-red-600'/> by Anubhav
             </div>
-            <div className='flex items-center gap-1'>
+            <div className='flex items-center gap-3'>
                 Connect with me :
-                <a href="https://www.linkedin.com/in/anubhav-baghel/" target="_blank"><FaLinkedin className='text-blue-600' /></a>
-                <a href="https://github.com/anubhavbaghel" target='_blank'><FaGithub className='text-[#1F2328]'/></a>
+                <a href="https://www.linkedin.com/in/anubhav-baghel/" target="_blank"><FaLinkedin className='text-blue-600 text-xl' /></a>
+                <a href="https://github.com/anubhavbaghel" target='_blank'><FaGithub className='text-[#1F2328] text-xl'/></a>
             </div>
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-center py-2 px-4'>
+    <nav className='flex justify-center py-2 px-4 sticky top-0 bg-white'>
         <div className="logo">
             <span className='text-xl font-medium'>Task Flow</span>
         </div>
